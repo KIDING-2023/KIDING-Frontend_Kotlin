e7c40492fffbeb1fe722b4a0ec7632b760304756
Siwon Jeon <siwonjeon234@gmail.com>
2023-11-19 11:45:51 +0900

    FEAT: 뷰 바인딩 활성화

--- app/src/main/java/com/example/kiding/LoginActivity.kt
+++ app/src/main/java/com/example/kiding/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.kiding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kiding.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
