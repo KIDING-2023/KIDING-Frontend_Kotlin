7222372fdbb13845fd26559259b978a363c5ed86
Siwon Jeon <siwonjeon234@gmail.com>
2023-12-24 11:58:08 +0900

    FEAT: 인기순, 최근순 (임시)

--- app/src/main/java/com/example/kiding/SortByPopularityFragment.kt
+++ app/src/main/java/com/example/kiding/SortByPopularityFragment.kt
@@ -1,19 +1,45 @@
 package com.example.kiding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kiding.databinding.FragmentSortByMainBinding
+import com.example.kiding.databinding.FragmentSortByPopularityBinding
 
 class SortByPopularityFragment : Fragment() {
+
+    private lateinit var binding: FragmentSortByPopularityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sort_by_popularity, container, false)
+        binding = FragmentSortByPopularityBinding.inflate(inflater, container, false)
+
+        binding.kikisday.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, KikisdayPlayActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        binding.space.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, SpacePlayActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return binding.root
     }
 
 }
